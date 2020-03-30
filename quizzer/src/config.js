7c2c80af9399c2a3173901b17e79d8d968c22d3e
Paul <PabloDiablo@users.noreply.github.com>
2020-03-30 22:27:33 +0100

    Added server debug script. Added dialog on window unload.

--- quizzer/src/config.js
+++ quizzer/src/config.js
@@ -1,5 +1,9 @@
-//SERVER URL + PORT
+// Production
 const URL = window.location.hostname;
 const httpHostname = `http://${URL}/api`;
+
+// Dev
+// const URL = "localhost:3001";
+// const httpHostname = `http://${URL}`;
 
 module.exports = { URL, httpHostname };
--- server/seed.js
+++ server/seed.js
@@ -6,6 +6,8 @@ const Games = mongoose.model("Games");
 const Questions = mongoose.model("Questions");
 const dbConfig = require("./config");
 
+const data = require("./data.json");
+
 mongoose
   .connect(
     `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}?retryWrites=true&w=majority`,
@@ -35,43 +37,5 @@ async function seedGames() {
 async function seedQuestions() {
   await Questions.deleteMany();
 
-  await Questions.insertMany([
-    {
-      question: "What's my name?",
-      answer: "Bob",
-      category: "General Knowledge"
-    },
-    {
-      question: "What colour is this?",
-      image:
-        "https://us.123rf.com/450wm/alkestida/alkestida1510/alkestida151000004/47412317-stock-vector-vector-green-apple-with-green-leaf-isolated-on-a-white-background.jpg?ver=6",
-      answer: "Green",
-      category: "General Knowledge"
-    },
-    {
-      question: "What channel is BBC One?",
-      answer: "one",
-      category: "General Knowledge"
-    },
-    {
-      question: "Who's this?",
-      answer: "Steve",
-      category: "General Knowledge"
-    },
-    {
-      question: "What's in my hand?",
-      answer: "Jeff",
-      category: "General Knowledge"
-    },
-    {
-      question: "Who sang this song?",
-      answer: "Fred",
-      category: "Music"
-    },
-    {
-      question: "What's this song?",
-      answer: "Sandi",
-      category: "Music"
-    }
-  ]);
+  await Questions.insertMany(data);
 }
