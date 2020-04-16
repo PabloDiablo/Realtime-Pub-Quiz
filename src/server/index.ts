f09d06a41b55b20de504fa838f240faf12ba52c1
Ricki Hastings <rickihastings22@gmail.com>
2020-04-16 21:53:22 +0100

    Adding socketId for debugging purposes

--- src/server/index.ts
+++ src/server/index.ts
@@ -56,7 +56,7 @@ websocketServer.on('connection', (socket, req: IncomingSocketMessage) => {
   const scoreBoard = req.session.scoreBoard;
   const teamName = req.session.teamName;
 
-  console.log(`Socket connected: ${teamName}`);
+  console.log(`Socket connected: ${teamName} (${socketId})`);
 
   //Als er een session is met een gameRoomName zet je de gameRoomName in de socket
   if (gameRoom) {
