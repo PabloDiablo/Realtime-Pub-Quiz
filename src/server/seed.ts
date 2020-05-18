3a10ac525d5c92c5f99485b214424e21ff2d9dcc
Paul Crane <paul.crane@bbc.co.uk>
2020-05-18 18:31:18 +0100

    Fixed seed script

--- src/server/seed.ts
+++ src/server/seed.ts
@@ -1,40 +1,53 @@
 import mongoose from 'mongoose';
+
 import dbConfig from './config';
+import AvailableQuestions from './database/model/availableQuestions';
+import Games from './database/model/games';
+import Questions from './database/model/questions';
+import Rounds from './database/model/rounds';
+import TeamAnswers from './database/model/teamAnswer';
+import Teams from './database/model/teams';
 
-require('./database/model/games');
-require('./database/model/questions');
+const argv = process.argv;
+const hasResetAllOption = argv.includes('--reset-all');
 
-const db = mongoose.connection;
-const Games = mongoose.model('Games');
-const Questions = mongoose.model('Questions');
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const data = require('./data.json');
+
+async function run() {
+  if (hasResetAllOption) {
+    console.log('Resetting database...');
+    await Promise.all([
+      AvailableQuestions.deleteMany({}),
+      Games.deleteMany({}),
+      Questions.deleteMany({}),
+      Rounds.deleteMany({}),
+      TeamAnswers.deleteMany({}),
+      Teams.deleteMany({})
+    ]);
+  } else {
+    console.log('Resetting available questions...');
+    await AvailableQuestions.deleteMany({});
+  }
+
+  console.log('Inserting new question data...');
+  await AvailableQuestions.insertMany(data);
+}
 
 mongoose
   .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
   })
   .then(() => {
-    console.log('connection succesful');
-    return seedQuestions();
+    console.log(`Connected to database ${dbConfig.DB}`);
+
+    return run();
   })
+  .catch(console.error)
   .then(() => {
-    return seedGames();
-  })
-  .catch(err => {
-    console.log(err);
-  })
-  .then(() => {
-    db.close();
+    mongoose.connection.close();
+    console.log('Done');
   });
-
-async function seedGames() {
-  await Games.deleteMany({});
-}
-
-async function seedQuestions() {
-  await Questions.deleteMany({});
-
-  await Questions.insertMany(data);
-}
