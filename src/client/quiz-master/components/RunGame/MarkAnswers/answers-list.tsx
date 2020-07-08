1a059f6c90b47eb62044aec03f836a2a7c165939
Paul <PabloDiablo@users.noreply.github.com>
2020-07-08 19:58:06 +0100

    Fixed team answers loading state

--- src/client/quiz-master/components/RunGame/MarkAnswers/answers-list.tsx
+++ src/client/quiz-master/components/RunGame/MarkAnswers/answers-list.tsx
@@ -96,58 +96,58 @@ const AnswersList: React.FC<Props> = ({ gameId, questionId, teams }) => {
 
   const classes = useStyles();
 
-  if (isLoading) {
-    return (
-      <div className={classes.loading}>
-        <CircularProgress />
-      </div>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <div className={classes.loading}>
+  //       <CircularProgress />
+  //     </div>
+  //   );
+  // }
 
   const firstCorrectTeamId = data
     .filter(a => a.timestamp !== undefined)
     .sort((a, b) => a.timestamp - b.timestamp)
     .find(a => a.isCorrect)?.teamId;
 
-  if (!isLoading) {
-    return (
-      <TableContainer component={Paper} className={classes.questionCard}>
-        <Table>
-          {data.length === 0 && (
-            <TableBody>
-              <TableRow>
-                <TableCell className={classes.waitingCell}>Waiting for answers...</TableCell>
-              </TableRow>
-            </TableBody>
-          )}
-          {data.length > 0 && (
-            <TableHead>
-              <TableRow>
-                <TableCell align="left" style={{ width: '200px' }}>
-                  Team
-                </TableCell>
-                <TableCell>Answer</TableCell>
-                <TableCell align="right" style={{ width: '150px' }} />
-              </TableRow>
-            </TableHead>
-          )}
+  // if (!isLoading) {
+  return (
+    <TableContainer component={Paper} className={classes.questionCard}>
+      <Table>
+        {data.length === 0 && (
           <TableBody>
-            {data.map(answer => (
-              <TeamAnswer
-                key={answer.id}
-                answer={answer}
-                teamAnswerId={answer.id}
-                isFirstCorrectAnswer={firstCorrectTeamId === answer.teamId}
-                team={teams.find(t => t.teamId === answer.teamId)}
-                gameStatus={gameStatus}
-                setTeamAnswer={setTeamAnswer}
-              />
-            ))}
+            <TableRow>
+              <TableCell className={classes.waitingCell}>Waiting for answers...</TableCell>
+            </TableRow>
           </TableBody>
-        </Table>
-      </TableContainer>
-    );
-  }
+        )}
+        {data.length > 0 && (
+          <TableHead>
+            <TableRow>
+              <TableCell align="left" style={{ width: '200px' }}>
+                Team
+              </TableCell>
+              <TableCell>Answer</TableCell>
+              <TableCell align="right" style={{ width: '150px' }} />
+            </TableRow>
+          </TableHead>
+        )}
+        <TableBody>
+          {data.map(answer => (
+            <TeamAnswer
+              key={answer.id}
+              answer={answer}
+              teamAnswerId={answer.id}
+              isFirstCorrectAnswer={firstCorrectTeamId === answer.teamId}
+              team={teams.find(t => t.teamId === answer.teamId)}
+              gameStatus={gameStatus}
+              setTeamAnswer={setTeamAnswer}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+  // }
 };
 
 export default AnswersList;
